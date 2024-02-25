11674fae0674661ca01d1aae25395a5169d26f75
TaeKyungKim <64403357+twingay96@users.noreply.github.com>
2024-02-25 22:13:45 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,10 +1,29 @@
-import numpy as np
-import pandas as pd
-import matplotlib.pylab as plt
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
 
-dfLoad = pd.read_csv("https://sites.google.com/site/vlsicir/testData_LinearRegression.txt",sep="\s+")
+This is a temporary script file.
+"""
+
+import math as m
+import numpy as np
+import matplotlib.pylab as plt
+import pandas as pd
+
+plt.close("all")
+dfLoad = pd.read_csv("https://raw.githubusercontent.com/hanwoolJeong/lectureUniv/main/testData_LinearRegression.txt"
+                     ,sep="\s+")
 xxRaw = dfLoad["xx"]
 yyRaw = dfLoad["yy"]
-yyRaw = np.array(yyRaw)
-plt.plot(xxRaw, yyRaw, "r.")
+yyRawNP = np.array(yyRaw)
+plt.plot(xxRaw,yyRaw, "r.")
 
+#Normal Equation
+Ndata = len(xxRaw)
+X = np.c_[np.ones([Ndata, 1]),xxRaw]
+wOLS = np.linalg.inv(X.T.dot(X)).dot(X.T).dot(yyRawNP.reshape(Ndata,1))
+xPredict = np.linspace(0,2,num=101)
+xPredictPadding = np.c_[np.ones([101,1]), xPredict]
+yPredict = wOLS.T.dot(xPredictPadding.T)
+
+plt.plot(xPredict.reshape(1,101),yPredict, "b.-")
